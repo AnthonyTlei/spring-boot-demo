44e0a159299142466b9ad9869cf5ed06254d187e
Anthony Tleiji <anthonytleiji@gmail.com>
2023-09-15 17:35:33 +0300

    Init Created Student Class Mapped Student to PostgreSQL Created Student Controller Created Student Service Created Student Repository Added GET endpoint: /student

---
+++ src/test/java/com/anthonytlei/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthonytlei.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
