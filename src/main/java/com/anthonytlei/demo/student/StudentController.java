3538267c34410c8b71e6feeec774820c7b46b11e
Anthony Tleiji <anthonytleiji@gmail.com>
2023-09-19 16:12:50 +0300

    Added Delete endpoint. Added Update endpoint.

--- src/main/java/com/anthonytlei/demo/student/StudentController.java
+++ src/main/java/com/anthonytlei/demo/student/StudentController.java
@@ -2,10 +2,14 @@ package com.anthonytlei.demo.student;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,5 +30,17 @@ public class StudentController {
     @PostMapping
     public void createStudent(@RequestBody Student student) {
         studentService.createStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+                studentService.updateStudent(studentId, name, email);
     }
 }
--- src/main/java/com/anthonytlei/demo/student/StudentService.java
+++ src/main/java/com/anthonytlei/demo/student/StudentService.java
@@ -1,9 +1,12 @@
 package com.anthonytlei.demo.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class StudentService {
@@ -24,6 +27,30 @@ public class StudentService {
             throw new IllegalStateException("Email taken");
         }
         studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long id) {
+        boolean exists = studentRepository.existsById(id);
+        if (!exists) {
+            throw new IllegalStateException(("Student with id: " + id + " does not exist"));
+        }
+        studentRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student with id: " + studentId + " does not exist"));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studeOptional = studentRepository.findByEmail(email);
+            if (studeOptional.isPresent()) {
+                throw new IllegalStateException("Email: " + email + " is already taken");
+            }
+            student.setEmail(email);
+        }
     }
 
 }
